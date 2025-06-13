9a1225d0f5fef71460de8a4983b4f408176f6ad7
Crab <dannywhetzel@gmail.com>
2025-06-13 09:44:42 -0400

    Added Wolfe's Incineroar support

--- SysBot.Pokemon.Discord/Commands/General/InfoModule.cs
+++ SysBot.Pokemon.Discord/Commands/General/InfoModule.cs
@@ -18,6 +18,8 @@ public class InfoModule : ModuleBase<SocketCommandContext>
     private const string detail = "I am an open-source Discord bot powered by PKHeX.Core and other open-source software.";
     private const string repo = "https://github.com/Manu098vm/SysBot.NET";
     private const string upstream = "https://github.com/kwsch/SysBot.NET";
+    private const string repo2 = "https://github.com/ThatOneCrab/placeholder-name";
+    private const string buildversion = "1.0.0";
 
     [Command("info")]
     [Alias("about", "whoami", "owner")]
@@ -27,12 +29,14 @@ public class InfoModule : ModuleBase<SocketCommandContext>
 
         var builder = new EmbedBuilder
         {
+            Title = "Here's a bit about me!",
             Color = new Color(114, 137, 218),
+            ThumbnailUrl = "https://media.discordapp.net/attachments/1234791557396172874/1383064282748682313/72x72_pokeball.png?ex=684d6e7d&is=684c1cfd&hm=20b42111f140319c8ab3933670156cecba8a1f4a1c5e39159c1671b00d8f228f&=&format=webp&quality=lossless.png",
             Description = detail,
         };
 
         builder.AddField("Info",
-            $"- [This fork Source Code]({repo}) by manu098vm\n" +
+            $"- [This fork Source Code]({repo2}) by Crab and Reedy built off of [This fork Source Code]({repo}) by manu098vm\n" +
             $"Thanks to notzyro, santacrab2, berichan and 9Bitdo for portions of code, help and updating.\n" +
             $"- [Upstream Source Code]({upstream}) by kwsch\n" +
             $"Thanks to Kurt, Anubis and Architdate for writing the original SysBot code.\n" +
@@ -42,6 +46,7 @@ public class InfoModule : ModuleBase<SocketCommandContext>
             $"- {Format.Bold("Runtime")}: {RuntimeInformation.FrameworkDescription} {RuntimeInformation.ProcessArchitecture} " +
             $"({RuntimeInformation.OSDescription} {RuntimeInformation.OSArchitecture})\n" +
             $"- {Format.Bold("Buildtime")}: {GetVersionInfo("SysBot.Base", false)}\n" +
+            $"- {Format.Bold("Build Version")}: {buildversion}\n" +
             $"- {Format.Bold("Core Version")}: {GetVersionInfo("PKHeX.Core")}\n" +
             $"- {Format.Bold("AutoLegality Version")}: {GetVersionInfo("PKHeX.Core.AutoMod")}\n"
         );
@@ -53,7 +58,7 @@ public class InfoModule : ModuleBase<SocketCommandContext>
             $"- {Format.Bold("Users")}: {Context.Client.Guilds.Sum(g => g.MemberCount)}\n"
         );
 
-        await ReplyAsync("Here's a bit about me!", embed: builder.Build()).ConfigureAwait(false);
+        await ReplyAsync("", embed: builder.Build()).ConfigureAwait(false);
     }
 
     private static string GetUptime() => (DateTime.Now - Process.GetCurrentProcess().StartTime).ToString(@"dd\.hh\:mm\:ss");
